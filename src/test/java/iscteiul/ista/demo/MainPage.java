f4645b3c5034ef6c0db519a9487956f0e03a3565
Pedro Henriques <prhss@iscte-iul.pt>
2025-12-03 20:48:21 +0100

    testes corrigidos

--- src/test/java/iscteiul/ista/demo/MainPage.java
+++ src/test/java/iscteiul/ista/demo/MainPage.java
@@ -1,14 +1,19 @@
 package iscteiul.ista.demo;
 
+import java.time.Duration;
+
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
+import static com.codeborne.selenide.Condition.visible;
+
 // page_url = https://www.jetbrains.com/
 public class MainPage {
-    public SelenideElement seeDeveloperToolsButton = $x("//*[@data-test-marker='Developer Tools']");
-    public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
-    public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
+  //  public SelenideElement seeDeveloperToolsButton = $x("//*[@data-test-marker='Developer Tools']");
+    public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-link']");
+  //  public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
     public SelenideElement searchButton = $("[data-test='site-header-search-action']");
+    public SelenideElement toolsMenu = $("[aria-label='Developer Tools: Open submenu']");
 }
